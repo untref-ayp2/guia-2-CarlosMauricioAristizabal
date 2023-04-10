a6c7bcda2192cbc149d74119552f216f006edf2f
CarlosMauricioAristizabal <cmabueno@gmail.com>
2023-04-09 23:50:02 -0300

    encapsulado

--- ejercicios/ejercicios.go
+++ ejercicios/ejercicios.go
@@ -6,16 +6,23 @@ import (
 
 func InvertirCadena(string) string {
 	//TODO
+	string := "borar"
+	return string
 }
 
 func Palindromo(string) bool {
 	//TODO
+	bool := true
+	return bool
 }
 
 func Balanceada(string) bool {
 	//TODO
+	bool := true
+	return bool
 }
 
 func UnirColas(q1, q2 queue.Queue) queue.Queue {
 	// TODO
+	return queue.Queue{}
 }
--- queue/queue.go
+++ queue/queue.go
@@ -1,53 +1,59 @@
 package queue
 
-import "errors"
+import (
+	"errors"
+	"guia2/stack"
+)
 
 // Queue implementa una cola genérica sobre un arreglo dinámico.
-type Queue []any
+type Queue struct {
+	cola []any
+}
 
 // Enqueue agrega un elemento a la cola. O(1)
 func (q *Queue) Enqueue(v any) {
-	*q = append(*q, v)
+	q.cola = append(q.cola, v)
 }
 
 // Dequeue saca un elemento de la cola. O(1)
 func (q *Queue) Dequeue() (any, error) {
-	if len(*q) == 0 {
+	if len(q.cola) == 0 {
 		return nil, errors.New("queue is empty")
 	}
-	head := (*q)[0]
-	*q = (*q)[1:]
+	head := (q.cola)[0]
+	q.cola = (q.cola)[1:]
 	return head, nil
 }
 
 // Front devuelve el elemento del frente de la cola. O(1)
 func (q *Queue) Front() (any, error) {
-	if len(*q) == 0 {
+	if q.IsEmpty() {
 		return nil, errors.New("queue is empty")
 	}
-	return (*q)[0], nil
+	return q.cola[0], nil
 }
 
 // IsEmpty verifica si la cola esta vacia. O(1)
 func (q *Queue) IsEmpty() bool {
-	return len(*q) == 0
+	return len(q.cola) == 0
 }
 
-
-type QueueS //TODO
+type QueueS struct {
+	pila stack.Stack
+}
 
 func (q *QueueS) Enqueue(v any) {
-    //TODO
+	//TODO
 }
 
 func (q *QueueS) Dequeue() (any, error) {
-    //TODO
+	//TODO
 }
 
 func (q *QueueS) IsEmpty() bool {
-    //TODO
+
 }
 
 func (q *QueueS) Front() (any, error) {
-    //TODO
+	//TODO
 }
