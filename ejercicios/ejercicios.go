39c74565288dd27913e2e59d61256572df16c5a7
CarlosMauricioAristizabal <cmabueno@gmail.com>
2023-04-12 22:02:54 -0300

    ejercicios

--- ejercicios/ejercicios.go
+++ ejercicios/ejercicios.go
@@ -2,18 +2,26 @@ package ejercicios
 
 import (
 	"guia2/queue"
+	"guia2/stack"
 )
 
-func InvertirCadena(string) string {
-	//TODO
-	string := "borar"
-	return string
+func InvertirCadena(cadena string) string {
+	var pila stack.Stack
+	var cadenaInvertida string
+	for _, valor := range cadena {
+		pila.Push(string(valor))
+	}
+	for !pila.IsEmpty() {
+		valor, _ := pila.Pop()
+		cadenaInvertida += valor.(string)
+	}
+	return cadenaInvertida
 }
 
-func Palindromo(string) bool {
-	//TODO
-	bool := true
-	return bool
+func Palindromo(texto string) bool {
+	textoInvertido := InvertirCadena(texto)
+
+	return texto == textoInvertido
 }
 
 func Balanceada(string) bool {
@@ -23,6 +31,9 @@ func Balanceada(string) bool {
 }
 
 func UnirColas(q1, q2 queue.Queue) queue.Queue {
-	// TODO
-	return queue.Queue{}
+	for !q2.IsEmpty() {
+		valor, _ := q2.Dequeue()
+		q1.Enqueue(valor)
+	}
+	return q1
 }
